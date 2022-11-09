43288275e2cdc48787b03e4eee5fd4abbb8b3cef
João Antonio <hiukakamachi@gmail.com>
2022-11-08 22:13:09 -0300

    Add product CRUD

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,28 +1,107 @@
 import { FastifyInstance } from "fastify";
+import { prisma } from '../lib/prisma'
 
-interface paramsType {
-  id: string | null
+interface paramsProps {
+  id: string
+}
+
+interface userSchema {
+  name: string
+  price: string
+  description: string
 }
 
 export async function Product(fastify: FastifyInstance,) {
   fastify.get('/product', async (_, reply) => {
-    return reply.send([])
+    try {
+      const usersFound = await prisma.product.findMany()
+
+      return reply.status(200).send(usersFound)
+    } catch (error) {
+      return reply.status(500).send({
+        message: "Servidor error",
+        error
+      })
+    }
+  })
+
+  fastify.post('/product', async (request, reply) => {
+    try {
+      const { name, price, description } = request.body as userSchema
+
+      await prisma.product.create({
+        data: {
+          name,
+          price,
+          description
+        }
+      })
+
+      return reply.status(201).send()
+    } catch (error) {
+      return reply.status(500).send({
+        message: 'could not create the product',
+        error
+      })
+    }
   })
 
   fastify.get('/product/:id', async (request, reply) => {
-    const { id } = request.params as paramsType
+    try {
+      const { id } = request.params as paramsProps
 
-    return reply.send(`Get user ${id}`)
+      const userFound = await prisma.product.findUnique({
+        where: {
+          id
+        }
+      })
+
+      return reply.status(200).send(userFound)
+    } catch (error) {
+      return reply.status(404).send({
+        message: 'User not found',
+        error
+      })
+    }
   })
 
   fastify.put('/product/:id', async (request, reply) => {
-    const { id } = request.params as paramsType
+    try {
+      const { id } = request.params as paramsProps
+      const { name, price, description } = request.body as userSchema
 
-    return reply.send(`Get user ${id}`)
+      const userUpdated = await prisma.product.update({
+        where: { id },
+        data: {
+          name,
+          price,
+          description
+        }
+      })
+
+      return reply.status(200).send(userUpdated)
+    } catch (error) {
+      return reply.status(405).send({
+        message: 'You are not allowed to modify the product',
+        error
+      })
+    }
   })
 
   fastify.delete('/product/:id', async (request, reply) => {
-    const { id } = request.params as paramsType
-    return reply.send(`Delete user a ${id}`)
+    try {
+      const { id } = request.params as paramsProps
+
+      await prisma.product.delete({
+        where: { id }
+      })
+
+      return reply.status(200).send()
+    } catch (error) {
+      return reply.status(405).send({
+        message: 'You are not allowed to delete the product',
+        error
+      })
+    }
   })
 }
